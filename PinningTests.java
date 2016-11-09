7600ee45ea16c9bf1597a3a1561c5f0ca04c40bc
David Bickford <drb56@pitt.edu>
2016-11-08 21:27:02 -0500

    added a few new tests

--- PinningTests.java
+++ PinningTests.java
@@ -8,7 +8,7 @@ import java.util.concurrent.TimeUnit;
 
 public class PinningTests {
 
-    Monkey firstMonkey;
+    Monkey firstMonkey, secondMonkey, thirdMonkey;
     /**
      * Runs this setup before the actual tests
      *
@@ -17,18 +17,47 @@ public class PinningTests {
     @Before
     public void setUp() {
         firstMonkey = new Monkey();
+        secondMonkey = new Monkey();
+        thirdMonkey = new Monkey();
     }
 
     @Test
-    public void getFirstMonkeyTestPasses() throws Exception{
-        assertEquals(firstMonkey.getId(), 0);
+    public void getFirstMonkeyTestPasses() throws Exception {
+        int id = firstMonkey.getId();
+        assertEquals(firstMonkey.getId(), id);
     }
 
     @Test
-    public void getFirstMonkeyTestFails() throws Exception{
+    public void getFirstMonkeyTestFails() throws Exception {
         assertNotEquals(firstMonkey.getId(), 20);
     }
 
-    
+    @Test
+    public void stringifyResultsMonkey1To2() throws NoIdException {
+        int firstId = firstMonkey.getId();
+        int secondId = secondMonkey.getId();
+        String output = "//Round 1: Threw banana from Monkey (#" + firstId + " / ID "
+                + firstId + ") to Monkey (#" + secondId + " / ID " + secondId + ")";
+        assertEquals(MonkeySim.stringifyResults(1, firstMonkey, secondMonkey), output);
+    }
+
+    @Test
+    public void stringifyResultsMonkey1To3() throws NoIdException {
+        int firstId = firstMonkey.getId();
+        int secondId = thirdMonkey.getId();
+        String output = "//Round 1: Threw banana from Monkey (#" + firstId + " / ID "
+                + firstId + ") to Monkey (#" + secondId + " / ID " + secondId + ")";
+        assertEquals(MonkeySim.stringifyResults(1, firstMonkey, thirdMonkey), output);
+    }
+
+    @Test
+    public void stringifyResultsMonkey2To3() throws NoIdException {
+        int firstId = secondMonkey.getId();
+        int secondId = thirdMonkey.getId();
+        String output = "//Round 1: Threw banana from Monkey (#" + firstId + " / ID "
+                + firstId + ") to Monkey (#" + secondId + " / ID " + secondId + ")";
+        assertEquals(MonkeySim.stringifyResults(1, secondMonkey, thirdMonkey), output);
+    }
+
 
 }
